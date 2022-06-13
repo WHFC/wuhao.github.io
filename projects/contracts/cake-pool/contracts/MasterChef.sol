5f1bfec4e57455ac6df4c87d0ca13ea1d593521d
WHFC <576517573@qq.com>
2022-06-13 23:01:53 +0800

    更新cake-pool

--- projects/contracts/cake-pool/contracts/MasterChef.sol
+++ projects/contracts/cake-pool/contracts/MasterChef.sol
@@ -953,10 +953,10 @@ contract BEP20 is Context, IBEP20, Ownable {
 
 // CakeToken with Governance.
 contract CakeToken is BEP20('MockKiKi', 'MKIKI') {
-    // /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
-    // function mint(address _to, uint256 _amount) public onlyOwner {
-    //     _mint(_to, _amount);
-    // }
+    /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
+    function mint(address _to, uint256 _amount) public onlyOwner {
+        _mint(_to, _amount);
+    }
 }
 
 /**
